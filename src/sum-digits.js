3cc5453121d034604ab53a86552ded374b83876b
ankmsn <khomstyle@gmail.com>
2023-04-17 22:21:56 +0100

    feat: recursive, sort, sum, season

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  while (n.toString().length > 1) {
+    let acc = 0;
+    let digits = n.toString().split("");
+    for (let i = 0; i < digits.length; i++) {
+      acc += +digits[i];
+    }
+    n = acc;
+  }
+  return n;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
